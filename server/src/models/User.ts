b17f063fb3fa0aa6a135d7d60983a7b40072915d
shellyear <boolevafunkce@gmail.com>
2024-12-31 03:09:33 +0500

    [server][chore] add profilePicture and photos to User schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,14 +1,20 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+type IImageUrl = string;
+
 export interface IUser extends Document {
   _id: string;
   phoneNumber: string;
   username?: string;
+  profilePicture?: IImageUrl;
+  photos?: IImageUrl[];
 }
 
 const UserSchema = new Schema<IUser>({
   phoneNumber: { type: String, required: true, unique: true },
   username: { type: String, unique: true },
+  profilePicture: { type: String },
+  photos: [{ type: String }],
 });
 
 const User = mongoose.model<IUser>("User", UserSchema);
