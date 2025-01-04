3075c60d475837bc182f9bac0be066ba4dfe1e57
shellyear <boolevafunkce@gmail.com>
2025-01-05 03:16:27 +0500

    [fix] user login page blinkage when user tries to access it while authorized

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
 
 import LoginForm from './LoginForm'
 import VerificationSubmit from './VerificationSubmit'
 import { useAuth } from '../../contexts/AuthContext'
 
 function Login() {
-  const navigate = useNavigate()
-  const { user } = useAuth()
+  const { user, loading } = useAuth()
   const [verificationCodeSent, setVerificationCodeSent] = useState(false)
 
-  useEffect(() => {
-    if (user) {
-      navigate('/')
-    }
-  }, [user, navigate])
+  if (loading) {
+    return null
+  }
+
+  if (user) {
+    return <Navigate to="/" />
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center">
