fd0655e86332af7c8cac177955a208adfe5a3d8b
shellyear <boolevafunkce@gmail.com>
2025-01-05 05:34:58 +0500

    [chore]

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/login", authController.login);
 router.post("/verify", authController.verify);
 router.get("/session", authController.session);
-router.post("/logout");
+router.post("/logout", authController.logout);
 
 export default router;
