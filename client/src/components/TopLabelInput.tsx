0ca71a7a1f01cdddeb203a6a6150a63cde6c8823
shellyear <boolevafunkce@gmail.com>
2025-02-01 21:53:59 +0500

    [fix] label font-semibold when input is focused, using peer

--- client/src/components/TopLabelInput.tsx
+++ client/src/components/TopLabelInput.tsx
@@ -27,12 +27,6 @@ function TopLabelInput({
 }: TopLabelInputProps) {
   return (
     <div className="relative mb-4 w-full">
-      <label
-        htmlFor={name}
-        className={`absolute -top-2 left-2 px-1 text-xs bg-white transition-colors tracking-tighter text-gray-600 focus:text-blue-500 ${labelClassname}`}
-      >
-        {label}
-      </label>
       <input
         type={type}
         name={name}
@@ -42,8 +36,14 @@ function TopLabelInput({
         minLength={minLength}
         maxLength={maxLength}
         onChange={onChange}
-        className={`w-full h-[54px] px-4 py-3 text-gray-900 border rounded-md focus:outline-none transition-colors border-gray-300 focus:border-blue-500 focus:bg-white focus:border-2 ${inputClassname}`}
+        className={`peer w-full h-[54px] px-4 py-3 text-gray-900 border rounded-md focus:outline-none transition-colors border-gray-300 focus:border-blue-500 focus:bg-white focus:border-2 ${inputClassname}`}
       />
+      <label
+        htmlFor={name}
+        className={`absolute -top-2 left-2 px-1 text-xs bg-white transition-colors tracking-tighter text-gray-600 focus:text-blue-500 peer-focus:font-semibold ${labelClassname}`}
+      >
+        {label}
+      </label>
     </div>
   )
 }
