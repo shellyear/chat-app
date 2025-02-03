b562302b34851c15b7ba60bb291162c8fd5eee80
shellyear <boolevafunkce@gmail.com>
2025-02-03 21:33:41 +0500

    [feature] add util to determine the id type

--- client/src/utils/chat.ts
+++ client/src/utils/chat.ts
@@ -7,3 +7,40 @@ export const generateChatLink = (username: string, userId: string) => {
     return `/${userId}`
   }
 }
+
+enum IdTypeEnum {
+  UNIQUE_NAME = 'UNIQUE_NAME',
+  USER = 'USER',
+  CHAT = 'CHAT',
+  CHANNEL = 'CHANNEL',
+  GROUP_CHAT = 'GROUP_CHAT'
+}
+
+export const getIdType = (id: string): IdTypeEnum | null => {
+  if (id.startsWith('@')) {
+    return IdTypeEnum.UNIQUE_NAME
+  }
+
+  if (id.startsWith('-')) {
+    // groupChat or ChannelId
+    const slicedId = id.slice(1)
+
+    if (slicedId.length === 10) {
+      return IdTypeEnum.CHANNEL
+    }
+
+    if (slicedId.length === 9) {
+      return IdTypeEnum.GROUP_CHAT
+    }
+  }
+
+  if (id.length === 9) {
+    return IdTypeEnum.USER
+  }
+
+  if (id.length === 10) {
+    return IdTypeEnum.CHAT
+  }
+
+  return null
+}
