90590eb09b3223994eab5d95499f7c69fb196228
shellyear <boolevafunkce@gmail.com>
2025-02-05 08:10:22 +0500

    [chore] show community info in top bar in chatArea

--- client/src/pages/dashboard/components/chatarea/ChatArea.tsx
+++ client/src/pages/dashboard/components/chatarea/ChatArea.tsx
@@ -1,13 +1,12 @@
 import { IoIosSend, IoMdArrowBack } from 'react-icons/io'
-import { Location, useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import { useEffect, useState } from 'react'
 
 import Avatar from '../../../../components/Avatar'
 import API from '../../../../api'
-import { IChat } from '../../../../types/chat'
+import { IChat, IChatParticipantInfo } from '../../../../types/chat'
 import useMsgPagination from '../../hooks/useMsgPagination'
-import { IUser } from '../../../../types/user'
 import { getIdMetadata, IdTypeEnum } from '../../../../utils/chat'
 
 function MessageInput() {
@@ -52,12 +51,8 @@ function Messages({ chatId }: { chatId: string }) {
 function ChatArea() {
   const { id } = useParams<{ id?: string }>()
   const navigate = useNavigate()
-  const [participant, setParticipant] = useState<IUser>()
+  const [communityInfo, setCommunityInfo] = useState<IChatParticipantInfo>()
   const [chat, setChat] = useState<IChat>()
-
-  const fetchCommunity = async (uniqueName: string) => {
-    await API.uniqueName.getCommunityInfoByUniqueName(uniqueName)
-  }
 
   useEffect(() => {
     if (!id) return
@@ -65,8 +60,15 @@ function ChatArea() {
     if (!idMetadata) return
 
     if (idMetadata.type === IdTypeEnum.UNIQUE_NAME) {
-      fetchCommunity(idMetadata.id)
-      // fetch base on @uniquName
+      API.uniqueName
+        .getCommunityInfoByUniqueName(idMetadata.id)
+        .then(({ data }) => {
+          setCommunityInfo(data)
+        })
+        .catch((err) => {
+          // eslint-disable-next-line no-console
+          console.log(err)
+        })
     }
 
     if (idMetadata.type === IdTypeEnum.USER) {
@@ -96,27 +98,27 @@ function ChatArea() {
 
   return (
     <div className="relative flex-grow flex flex-col">
-      <div className="flex items-center justify-between w-full bg-white p-4 border-b border-gray-200 cursor-pointer">
-        <button type="button" className="sm:hidden" onClick={() => navigate(-1)}>
-          <IoMdArrowBack className="w-5 h-5" />
-        </button>
-        <div className="flex gap-4 items-center">
-          {participant?.profilePicture ? (
-            <img src={participant?.profilePicture} alt="profilePicture" className="w-10 h-10 rounded-full" />
-          ) : (
-            <Avatar name={participant?.name} size="sm" />
-          )}
+      {communityInfo && (
+        <div className="flex items-center justify-between w-full bg-white p-4 border-b border-gray-200 cursor-pointer">
+          <button type="button" className="sm:hidden" onClick={() => navigate(-1)}>
+            <IoMdArrowBack className="w-5 h-5" />
+          </button>
+          <div className="flex gap-4 items-center">
+            {communityInfo?.profilePicture ? (
+              <img src={communityInfo?.profilePicture} alt="profilePicture" className="w-10 h-10 rounded-full" />
+            ) : (
+              <Avatar name={communityInfo?.name} size="sm" />
+            )}
+            <div>
+              <p className="font-bold text-base leading-4">{communityInfo?.name}</p>
+              <p className="text-sm text-gray-500">last seen recently</p>
+            </div>
+          </div>
           <div>
-            <p className="font-bold text-base leading-4">
-              {participant?.name} {participant?.surname}
-            </p>
-            <p className="text-sm text-gray-500">last seen recently</p>
+            <BsThreeDotsVertical className="h-5 w-5 text-gray-500" />
           </div>
         </div>
-        <div>
-          <BsThreeDotsVertical className="h-5 w-5 text-gray-500" />
-        </div>
-      </div>
+      )}
       <div className="chat-background" />
       {chat?._id ? <Messages chatId={chat._id} /> : <div className="flex-grow overflow-y-auto p-4 space-y-4" />}
       <MessageInput />
