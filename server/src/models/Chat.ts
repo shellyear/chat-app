e5347a9485ca00e5642a1b78732c046b47e2be94
shellyear <boolevafunkce@gmail.com>
2025-02-10 09:32:18 +0500

    [fix] participantIds are of type number in Chat model

--- server/src/models/Chat.ts
+++ server/src/models/Chat.ts
@@ -11,7 +11,7 @@ const ChatSchema = new mongoose.Schema(
   {
     participantsIds: [
       {
-        type: String,
+        type: Number,
         ref: "User",
         required: true,
         validate: {
